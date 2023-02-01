02b3d93d8703f9cab7bdaae1c187f92574b997e9
Justin Robbins <Sjrobbins96@gmail.com>
2023-02-01 13:46:22 -0600

    Initial commit

---
+++ Who's_that_Pokemon.py
@@ -0,0 +1,80 @@
+import difflib
+import random
+
+pokemon = {
+	'bulbasaur': {
+  	'feet': 4,
+    'color1': 'blue-green',
+    'color2': 'none',
+    'type1': 'grass',
+    'type2': 'poison'
+  },
+    'ivysaur': {
+    'feet': 4,
+    'color1': 'blue-green',
+    'color2': 'red',
+    'type1': 'grass',
+    'type2': 'poison'
+  },
+    'venusaur': {
+    'feet': 4,
+    'color1': 'blue-green',
+    'color2': 'red',
+    'type1': 'grass',
+    'type2': 'poison'  
+  },
+    'charmander': {
+    'feet': 2,
+    'color1': 'orange',
+    'color2': 'yellow',
+    'type1': 'fire',
+    'type2': 'none'  
+  },
+    'charmeleon': {
+    'feet': 2,
+    'color1': 'red',
+    'color2': 'yellow',
+    'type1': 'fire',
+    'type2': 'none'  
+  },
+    'charizard': {
+    'feet': 2,
+    'color1': 'orange',
+    'color2': 'yellow',
+    'type1': 'fire',
+    'type2': 'flying'  
+  },
+    'squirtle': {
+    'feet': 2,
+    'color1': 'blue',
+    'color2': 'brown',
+    'type1': 'water',
+    'type2': 'none'  
+  },
+    'wartortle': {
+    'feet': 2,
+    'color1': 'purple',
+    'color2': 'brown',
+    'type1': 'water',
+    'type2': 'none'  
+  },
+    'blastoise': {
+    'feet': 2,
+    'color1': 'blue',
+    'color2': 'brown',
+    'type1': 'water',
+    'type2': 'none'  
+  },
+}
+
+# Get random pokemon
+randPokemonName = random.choice(list(pokemon.keys()))
+randPokemon = pokemon[randPokemonName]
+input = input('Enter a pokemon: ')
+closest_match = difflib.get_close_matches(input , pokemon)
+print(closest_match)
+
+
+
+
+
